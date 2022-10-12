01a207b3513122c83a241aa950dbbec4829c2429
johnsmith <iea2001@mail.ru>
2022-10-12 19:37:43 +0700

    Create Main function, read file and get result

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,35 @@
-package org.example;public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+package org.example;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) throws URISyntaxException {
+        URL resource = Main.class.getResource("/words.txt");
+        File file = new File(resource.toURI());
+
+        Trie trie = new Trie();
+        try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+            while (reader.readLine() != null) {
+                trie.insert(reader.readLine());
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        Scanner scanner = new Scanner(System.in);
+        String str = "";
+
+        System.out.println("Input word or letter");
+        str = scanner.nextLine();
+        System.out.println(trie.autocomplete(str));
+
+
     }
 }
