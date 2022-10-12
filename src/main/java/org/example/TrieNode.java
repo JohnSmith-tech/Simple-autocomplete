0083c975589320087c2a4ad84df37a68f8608e48
johnsmith <iea2001@mail.ru>
2022-10-12 19:38:32 +0700

    Create trie node

--- src/main/java/org/example/TrieNode.java
+++ src/main/java/org/example/TrieNode.java
@@ -1,2 +1,25 @@
-package org.example;public class TrieNode {
+package org.example;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TrieNode {
+    char data;
+    List<TrieNode> children = new ArrayList<>();
+    boolean isEnd = false;
+
+
+    TrieNode(char c) {
+        data = c;
+    }
+
+
+    //Time O(k), Space O(1), k is number of children of this node
+    TrieNode getChild(char c) {
+        if (children != null)
+            for (TrieNode child : children)
+                if (child.data == c)
+                    return child;
+        return null;
+    }
 }
